313ccd387c756c85a9e5cef38cf4cce3cceba4dd
ayaka <143330537+ayaka400@users.noreply.github.com>
2025-08-03 16:11:34 +0900

    アーティスト一覧＋代表曲一覧表示

--- mynotes_app/views/artist_search_views.py
+++ mynotes_app/views/artist_search_views.py
@@ -1,33 +1,44 @@
 import requests
 from django.shortcuts import render
 from django.conf import settings
-from django.templatetags.static import static
 import config.constants as const
+from .track_search_views import get_track_info
 
-# 各アーティスト名からアーティスト詳細を取得
-def get_artist_info(artist_name):
+
+# アーティスト名からその代表曲(5曲)を取得する
+def get_top_tracks(artist_name):
     params = {
-        'method': 'artist.getInfo',
+        'method': 'artist.getTopTracks',
         'artist': artist_name,
         'api_key': settings.LASTFM_API_KEY,
         'format': 'json',
+        'limit': 5,
         'autocorrect': 1,
     }
 
     try:
         res = requests.get(const.API_URL, params=params)
-        data = res.json().get('artist', {})
+        tracks = res.json().get('toptracks', {}).get('track', [])
 
-        return {
-            'name': data.get('name', artist_name),
-            'url': data.get('url',  ''),
-        }
+        result = []
+        for track in tracks:
+            track_name = track.get('name')
+            url = track.get('url', '')
+
+            # imageはget_track_info関数から取得           
+            image = get_track_info(track_name, artist_name).get('image')
+
+            result.append({
+                'track_name': track_name,
+                'image': image,
+                'url': url,
+            })
+
+        return result
+
     except Exception as e:
-        print(f"[ERROR] get_artis_info: {e}")
-        return {
-            'name': artist_name,
-            'url': '',
-        }
+        return []
+
 
 # 検索ワードから該当アーティスト一覧を取得
 def search_artist(request):
@@ -40,18 +51,29 @@ def search_artist(request):
         'artist': query,
         'api_key': settings.LASTFM_API_KEY,
         'format': 'json',
-        'limit': 10,        
+        'limit': 10,
+        'autocorrect': 1,
     }
 
     response = requests.get(const.API_URL, params=params)
     data = response.json()
 
+    # 検索ワードに該当するアーティスト一覧
     raw_artists = data.get('results', {}).get('artistmatches', {}).get('artist', [])
     artists = []
 
     for artist in raw_artists:
         name = artist.get('name')
-        detailed = get_artist_info(name) # アーティスト詳細情報取得関数を呼び出し
-        artists.append(detailed)
+        mbid = artist.get('mbid') or None
+        url = artist.get('url', '')
+
+        # 該当アーティストのトップ5曲を取得（関数呼び出し）
+        top_tracks = get_top_tracks(name)
+
+        artists.append({
+            'name': name,
+            'url': url,
+            'top_tracks': top_tracks,
+        })
 
     return render(request, "search_artist.html", {"artists": artists,"query": query})
